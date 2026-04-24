ac4786e837440cad4e9256af2fdb765ff0ff5501
Max Sharov <maxsharoff@gmail.com>
2026-04-24 17:13:43 +0200

    build(d20): add inertia adapter

--- apps/d20/assets/vite.config.mjs
+++ apps/d20/assets/vite.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { phoenixVitePlugin } from 'phoenix_vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   },
   optimizeDeps: {
     // https://vitejs.dev/guide/dep-pre-bundling#monorepos-and-linked-dependencies
-    include: ["phoenix", "phoenix_html", "phoenix_live_view"],
+    include: ["@inertiajs/svelte", "phoenix", "phoenix_html", "phoenix_live_view", "svelte"],
   },
   build: {
     manifest: true,
@@ -23,6 +24,7 @@ export default defineConfig({
   // LV Colocated JS and Hooks
   // https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.ColocatedJS.html#module-internals
   resolve: {
+    conditions: ["svelte", "browser", "import", "default"],
     alias: {
       "@": ".",
       "phoenix-colocated": `${process.env.MIX_BUILD_PATH}/phoenix-colocated`,
@@ -30,6 +32,7 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
+    svelte(),
     phoenixVitePlugin({
       pattern: /\.(ex|heex)$/
     })
--- apps/d20/lib/d20_web/router.ex
+++ apps/d20/lib/d20_web/router.ex
@@ -1,6 +1,16 @@
 defmodule D20Web.Router do
   use D20Web, :router
 
+  pipeline :inertia do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {D20Web.Layouts, :inertia_root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Inertia.Plug
+  end
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -15,7 +25,7 @@ defmodule D20Web.Router do
   end
 
   scope "/", D20Web do
-    pipe_through :browser
+    pipe_through :inertia
 
     get "/", PageController, :home
   end
